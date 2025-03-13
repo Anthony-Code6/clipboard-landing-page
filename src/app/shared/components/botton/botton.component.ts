0a53d34fa5cee69578655d77fa05b7f05908cdf9
Anthony-Code6 <anthony2000_honorio@outlook.com>
2025-03-13 01:54:54 -0500

    creacion de los componente imagen y boton

---
+++ src/app/shared/components/botton/botton.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+
+@Component({
+  selector: 'app-botton',
+  imports: [],
+  templateUrl: './botton.component.html',
+  styleUrl: './botton.component.scss',
+})
+export class BottonComponent {
+  type = input<'button'|'submit'>();
+  color = input<string>('')
+}
---
+++ src/app/shared/components/imagen/imagen.component.ts
@@ -0,0 +1,12 @@
+import { Component, input } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
+@Component({
+  selector: 'app-imagen',
+  imports: [NgOptimizedImage],
+  templateUrl: './imagen.component.html',
+  styleUrl: './imagen.component.scss',
+})
+export class ImagenComponent {
+  imgUrl = input<string>()
+  altImg = input<string>()
+}
